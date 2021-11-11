6033504cef9fec895f59277308a3901743a7b20c
Rog <1910500309@qq.com>
2021-11-12 00:16:59 +0800

    添加项目文件。

---
+++ CodeInject.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <Windows.h>
+#include<TlHelp32.h>
+#include <stdio.h>
+
+#define DEBUG 1
+
+#define INIT_CODEBUFFER(s) { sizeof(s), s }
+typedef struct _CodeBuffer
+{
+	SIZE_T BufferSize;
+	BYTE* pBuffer;
+
+}CodeBuffer;
+
+class CodeInject
+{
+public:
+	//远线程注入
+	static BOOL ZwCreateThreadExCodeInject(DWORD dwPid, CodeBuffer Buffer);
+	//通过挂起方式创建进程，HOOK程序入口点执行ShellCode
+	static BOOL CreateProcessCodeInject(const WCHAR* pszTarget, CodeBuffer Buffer);
+	//APC注入
+	static BOOL QueueUserAPCCodeInject(DWORD dwPid, CodeBuffer Buffer);
+};
+
+
